2ac8aad928d181b5f81f826b3f1ac58810df1d64
ssebastianoo <sebastianogirotto05@gmail.com>
2022-02-08 12:34:46 +0100

    mobile

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let sections = document.getElementsByClassName('section');
 let scrolled = 0;
 
+let buttons = document.getElementsByClassName('scroll-button');
+
 document.addEventListener("DOMContentLoaded", function () {
     twemoji.parse(document.body);
     setTimeout(() => {
@@ -9,6 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1000);
 });
 
+function scroll() {
+    if (scrolled > sections.length - 1) {
+        scrolled = 0;
+    } else if (scrolled < 0) {
+        scrolled = sections.length - 1;
+    }
+    sections[scrolled].scrollIntoView();
+}
 
 window.addEventListener('keydown', (e) => {
     e.preventDefault();
@@ -24,10 +34,19 @@ window.addEventListener('keydown', (e) => {
             scrolled--;
             break;
     }
-    if (scrolled > sections.length - 1) {
-        scrolled = 0;
-    } else if (scrolled < 0) {
-        scrolled = sections.length - 1;
-    }
-    sections[scrolled].scrollIntoView();
+    scroll();
 })
+
+for (i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case 'scrollUp':
+                scrolled--;
+                break;
+            case 'scrollDown':
+                scrolled++;
+                break
+        }
+        scroll();
+    })
+}
